70433c5b2f6bd03c039b0f50340571a65c507d8b
DeekshithVakkala <vakkala70@gmail.com>
2023-06-07 06:56:24 -0500

    commit

---
+++ 06-05-2023.js
@@ -0,0 +1,9 @@
+function displayDetails(){
+    var fname= document.getElementById('firstname').value
+    var lname= document.getElementById('lastname').value
+    var email= document.getElementById('email').value
+    var dob= document.getElementById('dob').value
+    var phone= document.getElementById('phoneno').value
+    var toDisplay=document.getElementById('text')
+    toDisplay.innerHTML=  The info provided is <br> Fname : ${firstname} <br> Lname: ${lastname}
+                            <br> email : ${email} <br> DOB: ${dob} <br> Phone : ${phoneno} ;
